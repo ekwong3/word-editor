0e5f144bc4703c484815fc30e229026381c60c82
ethankwong1 <ethankwong2014@gmail.com>
2023-06-24 12:05:34 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,152 @@
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const resizeImg = require("resize-img");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+
+// const isDev = process.env.NODE_ENV !== "production";
+const isDev = true;
+const isMac = process.platform === "darwin";
+
+let mainWindow;
+let aboutWindow;
+
+// Main Window
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1000 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // Show devtools automatically if in development
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+// About Window
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: "About Electron",
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// When the app is ready, create the window
+app.on("ready", () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  // Remove variable from memory
+  mainWindow.on("closed", () => (mainWindow = null));
+});
+
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  // {
+  //   label: 'File',
+  //   submenu: [
+  //     {
+  //       label: 'Quit',
+  //       click: () => app.quit(),
+  //       accelerator: 'CmdOrCtrl+W',
+  //     },
+  //   ],
+  // },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+// Respond to the resize image event
+ipcMain.on("file:edit", (e, options) => {
+  console.log(options);
+  editText(options);
+});
+
+// Resize and save image
+async function editText({ filePath, find, replace }) {
+  try {
+    fs.readFile(filePath, "utf8", function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+
+      var result = data.replace(find, replace);
+
+      fs.writeFile(filePath, result, "utf8", function (err) {
+        if (err) return console.log(err);
+      });
+    });
+
+    // Send success to renderer
+    mainWindow.webContents.send("file:done");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  if (!isMac) app.quit();
+});
+
+// Open a window if none are open (macOS)
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
---
+++ renderer/js/renderer.js
@@ -0,0 +1,93 @@
+const form = document.querySelector("#img-form");
+const doc = document.querySelector("#doc");
+const filename = document.querySelector("#filename");
+const replaceInput = document.querySelector("#replace");
+const findInput = document.querySelector("#find");
+
+// Load image and show form
+function loadFile(e) {
+  const file = e.target.files[0];
+
+  // Check if file is an image
+  if (!isFileText(file)) {
+    alertError("Please select an image");
+    return;
+  }
+
+  findInput.value = "";
+  replaceInput.value = "";
+  // Show form, image name
+  form.style.display = "block";
+  filename.innerHTML = doc.files[0].name;
+}
+
+// Make sure file is text
+function isFileText(file) {
+  const acceptedFileTypes = [
+    "text/plain",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  ];
+  return file && acceptedFileTypes.includes(file["type"]);
+}
+
+// Resize image
+function editText(e) {
+  e.preventDefault();
+
+  if (!doc.files[0]) {
+    alertError("Please upload an Word file");
+    return;
+  }
+
+  if (findInput.value === "" || replaceInput.value === "") {
+    alertError("Please enter valid strings");
+    return;
+  }
+
+  // Electron adds a bunch of extra properties to the file object including the path
+  const filePath = doc.files[0].path;
+  const find = findInput.value;
+  const replace = replaceInput.value;
+
+  ipcRenderer.send("file:edit", {
+    filePath,
+    find,
+    replace,
+  });
+}
+
+// When done, show message
+ipcRenderer.on("file:done", () => {
+  alertSuccess("Text edited!");
+});
+
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "green",
+      color: "white",
+      textAlign: "center",
+    },
+  });
+}
+
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "red",
+      color: "white",
+      textAlign: "center",
+    },
+  });
+}
+
+// File select listener
+doc.addEventListener("change", loadFile);
+// Form submit listener
+form.addEventListener("submit", editText);
